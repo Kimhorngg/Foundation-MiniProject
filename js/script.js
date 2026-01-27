5a3d3396ae9424d7e7809d3b6811b763e794ad0e
sovannreach.vorng <vongsovannreach681@gmail.com>
2026-01-27 11:21:37 +0700

    Update Video

--- js/script.js
+++ js/script.js
@@ -11,4 +11,27 @@ const btn = document.getElementById("menu-btn");
         }
 });
 
+let videoPlayed =  document.getElementById("videoDis");
+let videoFrame =  document.getElementById("vdo");
+let closeBtn =  document.getElementById("close");
+let contentDiv =  document.getElementById("content");
+let body = document.getElementById("body");
+let header = document.getElementById("header"); 
+let overlay = document.getElementById("overlay");
+videoPlayed.addEventListener("click", () => {
+    videoFrame.style.display = "block";
+    contentDiv.style.display = "block";
+    body.style.overflow = "hidden";
+    overlay.style.display = "block";
+   
+
+});
+
+closeBtn.addEventListener("click", () => {
+    videoFrame.style.display = "none";
+    contentDiv.style.display = "none";
+    body.style.overflow = "auto";
+    overlay.style.display = "none";
+    header.style.zIndex = "50";
+});
 

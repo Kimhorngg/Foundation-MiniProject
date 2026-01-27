89167769bf69c10fecb794710bb3aa38b3810137
sovannreach.vorng <vongsovannreach681@gmail.com>
2026-01-27 22:28:38 +0700

    Update

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+      const btn = document.getElementById("menu-btn");
+      const menu = document.getElementById("mobile-menu");
+      const nav = document.getElementById("navbar");
+
+      btn.addEventListener("click", () => {
+        menu.classList.toggle("hidden");
+
+        if (window.innerWidth < 1024) {
+          nav.classList.toggle("bg-[#e1ecde]");
+          nav.classList.toggle("bg-white");
+        }
+      });
